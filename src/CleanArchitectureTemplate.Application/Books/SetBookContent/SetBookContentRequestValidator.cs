371e1d4294fb43b4d1ac66421a8a3a551c66721a
Dyego Maas <dyego.maas@gmail.com>
2021-10-17 17:35:15 -0300

    Added validator for SetBookContentRequest.

---
+++ src/CleanArchitectureTemplate.Application/Books/SetBookContent/SetBookContentRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace CleanArchitectureTemplate.Application.Books.SetBookContent
+{
+    public class SetBookContentRequestValidator : AbstractValidator<SetBookContentRequest>
+    {
+        public SetBookContentRequestValidator()
+        {
+            RuleFor(x => x.Content)
+                .NotEmpty().WithMessage("Content is required. Cannot be empty.");
+        }
+    }
+}
