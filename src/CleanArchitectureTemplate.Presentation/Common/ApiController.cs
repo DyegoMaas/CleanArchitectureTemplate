bb039ff4cec9bb2c21ec1c50757868e52b6b473e
Dyego Maas <dyego.maas@gmail.com>
2021-10-16 22:56:50 -0300

    Created BookEditingController.

---
+++ src/CleanArchitectureTemplate.Presentation/Common/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitectureTemplate.Presentation.Common
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class ApiController : ControllerBase
+    {
+        protected IMediator Mediator { get; }
+
+        protected ApiController(IMediator mediator)
+        {
+            Mediator = mediator;
+        }
+    }
+}
---
+++ src/CleanArchitectureTemplate.Presentation/Controllers/BookEditingController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using CleanArchitectureTemplate.Application.Books.InsertBookMetadata;
+using CleanArchitectureTemplate.Presentation.Common;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitectureTemplate.Presentation.Controllers
+{
+    public class BookEditingController : ApiController
+    {
+        public BookEditingController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        [Route("metadata")]
+        public async Task<InsertBookMetadataResponse> InsertMetadata(InsertBookMetadataRequest request)
+        {
+            return await Mediator.Send(request);
+        }
+    }
+}
