5032e4a6939dffb020368889f17fbc26f2039c54
Dyego Maas <dyego.maas@gmail.com>
2021-10-16 22:35:28 -0300

    Added Serilog as the default logger.

--- src/CleanArchitectureTemplate.Presentation/Program.cs
+++ src/CleanArchitectureTemplate.Presentation/Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace CleanArchitectureTemplate.Presentation
 {
@@ -18,6 +13,7 @@ namespace CleanArchitectureTemplate.Presentation
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((_, configuration) => { })
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
